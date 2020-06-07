2a10e36f78b1120ae97b0911970adf8e52ec6c3d
nosharp <nosharp1@gmail.com>
2020-06-07 21:38:49 +0100

    Add Test class for classes.

---
+++ tests/test.lua
@@ -0,0 +1,15 @@
+class animal
+
+    function new()
+        this.name = name
+    end
+
+    function getname()
+        return this.name
+    end
+
+    function setname(name)
+        this.name = name
+    end
+
+end
