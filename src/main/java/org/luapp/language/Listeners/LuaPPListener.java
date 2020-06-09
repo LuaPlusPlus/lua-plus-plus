32ae45d6fc2a16dce00c43f4af2422bd2bbb5b85
nosharp <nosharp1@gmail.com>
2020-06-09 02:30:18 +0100

    Handle onContext Enter and Exit in the LuaPPListener interface.

--- src/main/java/org/luapp/language/Listeners/LuaPPListener.java
+++ src/main/java/org/luapp/language/Listeners/LuaPPListener.java
@@ -1,4 +1,18 @@
 package org.luapp.language.Listeners;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+
 public interface LuaPPListener {
+
+    /**
+     * Handles the time it enters the context.
+     * @param enterContext The actual context.
+     */
+    void onEnterContext(ParserRuleContext enterContext);
+
+    /**
+     * Handles the time it exists the context
+     * @param exitContext The actual context.
+     */
+    void onExitContext(ParserRuleContext exitContext);
 }
