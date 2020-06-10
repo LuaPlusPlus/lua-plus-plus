6663c92137c857a4fa0c7057aa6bc763d5735d2c
nosharp <nosharp1@gmail.com>
2020-06-10 12:22:42 +0100

    Update tests.

---
+++ src/main/java/org/luapp/language/test.lua
@@ -0,0 +1,17 @@
+--[[
+Written by nosharp (https://nosharp.cc),
+tom.bat (tomdotbat.dev),
+sammy milton (smilton.dev)
+]]--
+class animal
+    constructor(name)
+        self.name = name
+    end
+end
+self.name = name
+self.name = name
+class animal
+    constructor(name)
+        self.name = name
+    end
+end
